db02f48b26f8b105fa0a6a3995cacc08e3d80e3c
Alexander Heath <alexander_heath@apple.com>
2021-01-21 23:35:44 -0600

    Initial Commit

---
+++ Shared/inTouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  inTouchApp.swift
+//  Shared
+//
+//  Created by Alexander Heath on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct inTouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
